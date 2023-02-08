77d80f6a51f1d17bb4efe93004938c5ddcfef10e
Rafael Stahl <r.stahl@tum.de>
2023-02-08 20:44:34 +0100

    add uma integration

--- tvmc_wrapper.py
+++ tvmc_wrapper.py
@@ -4,6 +4,7 @@ from tvm.driver.tvmc.main import _main, register_parser
 from tvm.driver.tvmc.compiler import drive_compile, add_compile_arguments
 
 from disable_legalize import OptionallyDisableLegalize
+import uma_loader
 
 
 def drive_tumeda_compile(args):
@@ -21,7 +22,20 @@ def add_tumeda_compile_parser(subparsers, parser, json_params):
     group.add_argument(
         "--disable-legalize", action="store_true", help="apply a custom transformation to disable legalization passes."
     )
+    group.add_argument(
+        "--uma-backends", action="append", default=[], help="add directory to search for UMA backends."
+    )
+
+    # Need to manually parse UMA targets so that their target options can be generated automatically.
+    for i, arg in enumerate(sys.argv):
+        if arg == "--uma-backends" and i + 1 < len(sys.argv):
+            umadir = sys.argv[i + 1]
+            sys.path.insert(0, umadir)
+            sys.path.insert(0, umadir + "/uma_backends")
+    uma_loader.init()
+
     add_compile_arguments(parser, json_params)
+
 
 
 def main():
